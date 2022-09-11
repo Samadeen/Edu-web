3791ed38615bed804b77f135ec1d0b302c3ce6e5
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-11 07:53:33 +0100

    Header Component

---
+++ src/components/common/heading/Header.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
+
+import Head from './Head';
+import './Header.css';
+
+const Header = () => {
+  const [click, setClick] = useState(false);
+
+  return (
+    <>
+      <Head />
+      <header>
+        <nav className='flexSB'>
+          <ul
+            className={click ? 'mobile-nav' : 'flexSB'}
+            onClick={() => setClick(false)}
+          >
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/courses'>All Courses</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/team'>Team</Link>
+            </li>
+            <li>
+              <Link to='/pricing'>Pricing</Link>
+            </li>
+            <li>
+              <Link to='/journal'>Journal</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+          <div className='start'>
+            <button className='button'>Get Certificate</button>
+          </div>
+          <button className='toggle' onClick={() => setClick(!click)}>
+            {click ? <FaTimes /> : <FaBars />}
+          </button>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
