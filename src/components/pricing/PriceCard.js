971214ab7c5ee4fd16d10640ed056227c967444a
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-13 18:33:55 +0100

    Created Price card component

---
+++ src/components/pricing/PriceCard.js
@@ -0,0 +1,21 @@
+import { price } from '../../dummydata';
+
+const PriceCard = () => {
+  return (
+    <>
+      {price.map((val) => (
+        <div key={Math.random()} className='items shadow'>
+          <h4>{val.name}</h4>
+          <h1>
+            <span>$</span>
+            {val.price}
+          </h1>
+          <p>{val.desc}</p>
+          <button className='outline-btn'>GET STARTED</button>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default PriceCard;
