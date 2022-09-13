d22ac0547644acb740f2ecebc659cf3e3dd3a949
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-13 10:38:50 +0100

    Team Card Component

---
+++ src/components/team/TeamCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FaFacebookF, FaInstagram, FaTiktok, FaTwitter } from 'react-icons/fa';
+import { team } from '../../dummydata';
+
+const TeamCard = () => {
+  return (
+    <>
+      {team.map((val) => (
+        <div className='items shadow'>
+          <div className='img'>
+            <img src={val.cover} alt='cover' />
+            <div className='overlay'>
+              <FaFacebookF className='icon' />
+              <FaInstagram className='icon' />
+              <FaTwitter className='icon' />
+              <FaTiktok className='icon' />
+            </div>
+          </div>
+          <div className='details'>
+            <h2>{val.name}</h2>
+            <p>{val.work}</p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default TeamCard;
